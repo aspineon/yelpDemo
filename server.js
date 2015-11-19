83473037ff97279b0b10109a383dc4844009bfbe
muzir <baycelik_erhun@hotmail.com>
2015-11-20 01:02:56 +0200

    listed done sucessfully we need to get http request to run

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-// getting-started.js
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/test');
+
 var app = express();
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback) {
-  // yay!
+  console.log('Connection Opened');
 });
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public", {
@@ -18,34 +20,7 @@ var personSchema = mongoose.Schema({
   age: String
 });
 var Person = mongoose.model('Person', personSchema);
-app.get('/', function(req, resp) {
-  console.log('Get Request');
-  //var personSchema = mongoose.Schema({
-  //  name: String,
-  //  age: String
-  //});
-  //var Person = mongoose.model('Person', personSchema);
-  var persons = [];
-  Person.find(function(err, persons) {
-    if (err) return console.error(err);
-    console.log(persons);
-  })
-  resp.json(persons);
-  //resp.sendFile(__dirname + '/public/angular.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
-//app.get("/", function(req, resp) {
-//  console.log('Get Request');
-//  var personSchema = mongoose.Schema({
-//    name: String,
-//    age: String
-//  });
-//  var Person = mongoose.model('Person', personSchema);
-//  Person.find(function(err, persons) {
-//    if (err) return console.error(err);
-//    console.log(persons);
-//  })
-//  resp.json(persons);
-//});
+
 
 app.post("/person", function(req, resp) {
   var body = req.body;
@@ -53,9 +28,10 @@ app.post("/person", function(req, resp) {
   var paramAge = body.age;
   console.log('Person name:' + paramName);
   console.log('Person age:' + paramAge);
+
   var newPerson = new Person({
-    name: paramName,
-    age: paramAge
+    name:paramName,
+    age:paramAge
   });
   console.log('New Person name:' + newPerson.name);
   console.log('New Person age:' + newPerson.age);
@@ -63,13 +39,12 @@ app.post("/person", function(req, resp) {
     if (err) return console.error(err);
     console.log('success');
   });
-
-  var persons = "";
+  var persons = [];
   Person.find(function(err, persons) {
     if (err) return console.error(err);
     console.log(persons);
-  })
-  resp.json(persons);
+    resp.json(JSON.stringify(persons));
+  });
 });
 
 app.listen(8080);
