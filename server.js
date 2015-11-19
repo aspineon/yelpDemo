8b0d203b8cfb1f6d382c16887fba8ccfb63a2f3b
muzir <baycelik_erhun@hotmail.com>
2015-11-20 01:33:06 +0200

    add get request but not working now

--- server.js
+++ server.js
@@ -20,7 +20,15 @@ var personSchema = mongoose.Schema({
   age: String
 });
 var Person = mongoose.model('Person', personSchema);
-
+app.get("/", function(req, resp) {
+  console.log('Get request handled');
+  var persons = [];
+  Person.find(function(err, persons) {
+    if (err) return console.error(err);
+    console.log(persons);
+    resp.json(JSON.stringify(persons));
+  });
+});
 
 app.post("/person", function(req, resp) {
   var body = req.body;
@@ -30,8 +38,8 @@ app.post("/person", function(req, resp) {
   console.log('Person age:' + paramAge);
 
   var newPerson = new Person({
-    name:paramName,
-    age:paramAge
+    name: paramName,
+    age: paramAge
   });
   console.log('New Person name:' + newPerson.name);
   console.log('New Person age:' + newPerson.age);
