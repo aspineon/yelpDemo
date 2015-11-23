b91908a87358a78eab89c56039325de7fb20c97f
muzir <baycelik_erhun@hotmail.com>
2015-11-23 20:21:40 +0200

    Fix unnecessary jSON conversition

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ var personSchema = mongoose.Schema({
   age: String
 });
 var Person = mongoose.model('Person', personSchema);
-app.get("/person", function(req, resp) {
-  console.log('Get request handled');
+app.get("/",function(req,resp){
+console.log('Get request handled');
   var persons = [];
   Person.find(function(err, persons) {
     if (err) return console.error(err);
     console.log(persons);
-    resp.json(JSON.stringify(persons));
+    resp.json(persons);
   });
 });
 
@@ -38,8 +38,8 @@ app.post("/person", function(req, resp) {
   console.log('Person age:' + paramAge);
 
   var newPerson = new Person({
-    name: paramName,
-    age: paramAge
+    name:paramName,
+    age:paramAge
   });
   console.log('New Person name:' + newPerson.name);
   console.log('New Person age:' + newPerson.age);
@@ -51,7 +51,7 @@ app.post("/person", function(req, resp) {
   Person.find(function(err, persons) {
     if (err) return console.error(err);
     console.log(persons);
-    resp.json(JSON.stringify(persons));
+    resp.json(persons);
   });
 });
 
